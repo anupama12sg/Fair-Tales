5e96b8f925ac10b616b3152a3c59c1383f050208
Aditya <anupama12sg@gmail.com>
2025-07-20 18:01:00 +0530

    feat: created a local JSON file & added sample stories

---
+++ src/lib/sampleStories.js
@@ -0,0 +1,32 @@
+export const sampleStories = [
+  {
+    id: 'Qm1234abcd',  
+    title: 'The Freedom Tale',
+    author: '0x1234...abcd',
+    summary: 'A short story about freedom of expression.',
+  },
+  {
+    id: 'Qm5678efgh',
+    title: 'Blockchain Dreams',
+    author: '0x5678...efgh',
+    summary: 'Exploring how decentralization changes narratives.',
+  },
+  {
+    id: 'Qm5678ijkl',
+    title: 'Mission Impossible',
+    author: '0x5678...ijkl',
+    summary: 'Guide on how to do stunts like a professional.',
+  },
+  {
+    id: 'Qm5678mnop',
+    title: 'Home Alone',
+    author: '0x5678...mnop',
+    summary: 'Acing to stay at home but make it adventure.',
+  },
+  {
+    id: 'Qm5678qrst',
+    title: 'Legally Blonde',
+    author: '0x5678...qrst',
+    summary: 'Self- confidence can take you places. Just believe in yourself.',
+  },
+];
